bffd3840993c68c55de1075b5346df8dbd0d8486
loong_zhou <loong_zhou@trendmicro.com>
2023-03-29 17:17:49 +0800

    refine socket_redirect

--- socket-redirect/socket_redirect.bpf.c
+++ socket-redirect/socket_redirect.bpf.c
@@ -11,42 +11,6 @@ struct {
     __type(value, struct sock *);
 } sock_ops_map SEC(".maps");
 
-SEC("sockops")
-int sock_map_update(struct bpf_sock_ops *skops) {
-    struct sock_key key = {};
-    struct sock *sk;
-    int op;
-
-    sk = (struct sock *)skops->sk;
-    if (!sk)
-        return 0;
-
-    op = (int)skops->op;
-
-    // key.family = BPF_CORE_READ(sk, __sk_common.skc_family);
-    // key.sip4 = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
-    // key.dip4 = BPF_CORE_READ(sk, __sk_common.skc_daddr);
-    // key.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
-    // key.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
-    key.family = sk->sk_family;
-    key.sip4 = sk->sk_rcv_saddr;
-    key.sport = sk->sk_num;
-    key.dip4 = sk->sk_daddr;
-    key.dport = sk->sk_dport;
-
-    if (op == BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB || op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
-        bpf_sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
-    } else if (op == BPF_SOCK_OPS_STATE_CB) {
-        int state = skops->args[0];
-
-        if (state == TCP_CLOSE) {
-            bpf_map_delete_elem(&sock_ops_map, &key);
-        }
-    }
-
-    return 0;
-}
-
 // SEC("sockops")
 // int sock_map_update(struct bpf_sock_ops *skops) {
 //     struct sock_key key = {};
@@ -59,11 +23,11 @@ int sock_map_update(struct bpf_sock_ops *skops) {
 
 //     op = (int)skops->op;
 
-//     key.family = bpf_core_read(&sk->__sk_common.skc_family, sizeof(key.family));
-//     key.sip4 = bpf_core_read(&sk->__sk_common.skc_rcv_saddr, sizeof(key.sip4));
-//     key.dip4 = bpf_core_read(&sk->__sk_common.skc_daddr, sizeof(key.dip4));
-//     key.sport = bpf_core_read(&sk->__sk_common.skc_num, sizeof(key.sport));
-//     key.dport = bpf_core_read(&sk->__sk_common.skc_dport, sizeof(key.dport));
+//     key.family = BPF_CORE_READ(sk, __sk_common.skc_family);
+//     key.sip4 = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
+//     key.dip4 = BPF_CORE_READ(sk, __sk_common.skc_daddr);
+//     key.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
+//     key.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
 
 //     if (op == BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB || op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
 //         bpf_sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
@@ -77,6 +41,37 @@ int sock_map_update(struct bpf_sock_ops *skops) {
 
 //     return 0;
 // }
+
+SEC("sockops")
+int sock_map_update(struct bpf_sock_ops *skops) {
+    struct sock_key key = {};
+    struct sock *sk;
+    int op;
+
+    sk = (struct sock *)skops->sk;
+    if (!sk)
+        return 0;
+
+    op = (int)skops->op;
+
+    key.family = bpf_core_read(&sk->__sk_common.skc_family, sizeof(key.family));
+    key.sip4 = bpf_core_read(&sk->__sk_common.skc_rcv_saddr, sizeof(key.sip4));
+    key.dip4 = bpf_core_read(&sk->__sk_common.skc_daddr, sizeof(key.dip4));
+    key.sport = bpf_core_read(&sk->__sk_common.skc_num, sizeof(key.sport));
+    key.dport = bpf_core_read(&sk->__sk_common.skc_dport, sizeof(key.dport));
+
+    if (op == BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB || op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
+        bpf_sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
+    } else if (op == BPF_SOCK_OPS_STATE_CB) {
+        int state = skops->args[0];
+
+        if (state == TCP_CLOSE) {
+            bpf_map_delete_elem(&sock_ops_map, &key);
+        }
+    }
+
+    return 0;
+}
 
 SEC("sk_msg")
 int sendmsg_prog(struct sk_msg_md *msg) {
