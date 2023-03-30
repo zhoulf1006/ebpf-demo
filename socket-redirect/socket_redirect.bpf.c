54c86ed5ab8c368bbc71406d377e6c0442441a88
loong_zhou <loong_zhou@trendmicro.com>
2023-03-30 11:35:06 +0800

    debug socket_redirect

--- socket-redirect/socket_redirect.bpf.c
+++ socket-redirect/socket_redirect.bpf.c
@@ -28,20 +28,6 @@ static __always_inline void output_connection_info(void *ctx, struct sock_key *k
     bpf_ringbuf_output(&ring_buffer_map, &conn_info, sizeof(conn_info), BPF_RB_FORCE_WAKEUP);
 }
 
-// static inline void extract_key_from_sock_ops(struct bpf_sock_ops *skops, struct sock_key *key)
-// {
-//     key->family = BPF_CORE_READ(skops->sk, __sk_common.skc_family);
-//     key->sip4 = BPF_CORE_READ(skops->sk, __sk_common.skc_rcv_saddr);
-//     key->dip4 = BPF_CORE_READ(skops->sk, __sk_common.skc_daddr);
-//     key->sport = BPF_CORE_READ(skops->sk, __sk_common.skc_num);
-//     key->dport = BPF_CORE_READ(skops->sk, __sk_common.skc_dport);
-
-//     // 清除填充字段
-//     key->pad1 = 0;
-//     key->pad2 = 0;
-//     key->pad3 = 0;
-// }
-
 static __always_inline void sk_extract4_key(const struct bpf_sock_ops *ops,
 					    struct sock_key *key)
 {
@@ -94,31 +80,14 @@ int sock_map_update(struct bpf_sock_ops *skops) {
     struct sock *sk;
     int op;
 
-    bpf_trace_printk("sockops init finish\n", sizeof("sockops init finish\n"));
+    // bpf_trace_printk("sockops init finish\n", sizeof("sockops init finish\n"));
     sk = (struct sock *)skops->sk;
     if (!sk)
         return 0;
 
-    bpf_trace_printk("sockops sk finish\n", sizeof("sockops sk finish\n"));
-
     op = (int)skops->op;
 
-    bpf_trace_printk("sockops op finish\n", sizeof("sockops op finish\n"));
-
     sk_extract4_key(skops, &key);
-    // key.sip4 = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
-    // key.dip4 = BPF_CORE_READ(sk, __sk_common.skc_daddr);
-    // key.family = BPF_CORE_READ(sk, __sk_common.skc_family);
-    // key.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
-    // key.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
-
-    // __u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
-    // __u32 sip4 = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
-
-    // key.family = family;
-    // key.sip4 = sip4;
-
-    bpf_trace_printk("sockops BPF_CORE_READ finish\n", sizeof("sockops BPF_CORE_READ finish\n"));
 
     if (op == BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB || op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
         bpf_sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
@@ -139,12 +108,6 @@ int sendmsg_prog(struct sk_msg_md *msg) {
     struct sock_key key = {};
     struct sock *sk;
 
-    // key.family = msg->family;
-    // key.sip4 = msg->local_ip4;
-    // key.sport = msg->local_port;
-    // key.dip4 = msg->remote_ip4;
-    // key.dport = msg->remote_port;
-
     sk_msg_extract4_key(msg, &key);
 
     sk = bpf_map_lookup_elem(&sock_ops_map, &key);
@@ -153,7 +116,8 @@ int sendmsg_prog(struct sk_msg_md *msg) {
         return SK_PASS;
     }
 
-    return bpf_sk_redirect_map((struct __sk_buff *)msg->sk, &sock_ops_map, (unsigned long)&key, BPF_F_INGRESS);
+    // return bpf_sk_redirect_map((struct __sk_buff *)msg->sk, &sock_ops_map, (unsigned long)&key, BPF_F_INGRESS);
+    return bpf_msg_redirect_hash(msg, &sock_ops_map, &key, BPF_F_INGRESS);
 }
 
 char _license[] SEC("license") = "GPL";
